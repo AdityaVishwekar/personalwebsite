771678924d29344f0989c2ab7870b45957c06483
aditya <avishwekar10893@gmail.com>
2016-10-11 17:41:35 -0500

    Second Commit

---
+++ js/toggle.js
@@ -0,0 +1,11 @@
+$( "#parent" ).click(function() {
+  $( "#popup" ).toggle();
+});
+
+$( "#parent1" ).click(function() {
+  $( "#popup1" ).toggle();
+});
+
+$( "#parent2" ).click(function() {
+  $( "#popup2" ).toggle();
+});
