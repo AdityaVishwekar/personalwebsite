ea270103ef9ded605cba71d258d089438efbbc5a
aditya <avishwekar10893@gmail.com>
2016-10-15 19:22:58 -0500

    version.0.1

---
+++ js/common.js
@@ -0,0 +1,10 @@
+// Scrolling 
+
+function disableScrolling() {
+    document.body.style.overflow = 'hidden';
+}
+
+function enableScrolling() {
+    document.body.style.overflow = 'scroll';
+     window.my_mute = true;
+}
